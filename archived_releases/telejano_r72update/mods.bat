6aa815d7f5d719187ec79677f68ec38d0e80fc9c
RobyKalilla <77798681+RobyKalilla@users.noreply.github.com>
2021-12-10 15:50:57 +0100

    Added archived compiled versions of the code

---
+++ archived_releases/telejano_r72update/mods.bat
@@ -0,0 +1,7 @@
+@echo off
+
+echo RUNNING %1 MOD, %2 MAP
+telejano -data modelpack -data texturepack  -mod %1 +map %2 %4 %5 %6 
+
+
+
---
+++ archived_releases/telejano_r72update/normal.bat
@@ -0,0 +1,7 @@
+@echo off
+
+echo RUNNING TELEJANO WITH FULL EYECANDYPACKS
+telejano -data modelpack -data skinpack -data texturepack  %1 %2 %4 %5 %6 
+
+
+
